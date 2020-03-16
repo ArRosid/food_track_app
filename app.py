b85309eb8423951f9b4b5f34a5a57a2d445d79ea
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-16 16:13:10 +0700

    display all day

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, g, request
+from datetime import datetime
 import dbcon
 
 app = Flask(__name__)
@@ -8,9 +9,32 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template('home.html')
+    db = dbcon.get_db()
+
+    if request.method == "POST":
+        date = request.form["date"] # the format will be yyyy-mm-dd
+        
+        dt = datetime.strptime(date, "%Y-%m-%d")
+        database_date = datetime.strftime(dt, "%Y%m%d")
+        
+        db.execute("insert into log_date (entry_date) values (?)",
+                    [database_date])
+        db.commit()
+
+    cur = db.execute("select * from log_date order by entry_date desc")
+    results = cur.fetchall()
+
+    date_result = []
+
+    for r in results:
+        single_date = {}
+        d = datetime.strptime(str(r["entry_date"]), '%Y%m%d')
+        single_date["pretty_date"] = datetime.strftime(d, "%B %d, %Y") #%B for month in word
+        date_result.append(single_date)
+
+    return render_template('home.html', results=date_result)
 
 @app.route('/view')
 def view():
