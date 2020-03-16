6cffe5e1edc802d9a75b4bf7f18e323bbed04590
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-16 15:56:03 +0700

    Display all foods

--- app.py
+++ app.py
@@ -13,21 +13,25 @@ def view():
 
 @app.route("/food", methods=["GET","POST"])
 def food():
+
+    db = dbcon.get_db()
+   
     if request.method == "POST":
         food_name = request.form.get("food-name")
-        protein = request.form.get("protein")
-        carbohydrates = request.form.get("carbohydrates")
-        fat = request.form.get("fat")
+        protein = int(request.form.get("protein"))
+        carbohydrates = int(request.form.get("carbohydrates"))
+        fat = int(request.form.get("fat"))
 
         calories = protein * 4 + carbohydrates * 4 + fat * 9
-        
-        db = dbcon.get_db()
         db.execute("insert into food (name, protein, carbohydrates, fat, calories)\
                     values (?,?,?,?,?)",
                     [food_name, protein, carbohydrates, fat, calories])
         db.commit()
 
-    return render_template("add_food.html")
+    cur = db.execute("select * from food")
+    results = cur.fetchall()
+
+    return render_template("add_food.html", results=results)
 
 
 if __name__ == "__main__":
