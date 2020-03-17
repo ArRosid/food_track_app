514287e95c8400e5de0cda413f640f2d5706c03d
ArRosid <ahmadrosid30121997@gmail.com>
2020-03-17 10:27:42 +0700

    Display total food in day page

--- app.py
+++ app.py
@@ -65,8 +65,21 @@ def view(date):
                             where log_date.entry_date = ? ''', [date])
     log_results = log_cur.fetchall()
 
-    return render_template("day.html", entry_date=result["entry_date"], pretty_date=pretty_date,
-                            food_results=food_results, log_results=log_results)
+    totals = {}
+    totals['protein'] = 0
+    totals["carbohydrates"] = 0
+    totals["fat"] = 0
+    totals["calories"] = 0
+
+    for food in log_results:
+        totals["protein"] += food["protein"]
+        totals["carbohydrates"] += food["carbohydrates"]
+        totals["fat"] += food["fat"]
+        totals["calories"] += food["calories"]
+
+    return render_template("day.html", entry_date=result["entry_date"],
+                            pretty_date=pretty_date, food_results=food_results,
+                            log_results=log_results, totals=totals)
 
 @app.route("/food", methods=["GET","POST"])
 def food():
